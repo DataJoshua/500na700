1a5f613ed5e4859c397acb93c7fcbdc1850baf5a
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-04-22 22:30:10 +0400

    Added new folder structure and variables.scss created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 function App() {
   return (
     <>
-      <h1>Hello There</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quaerat voluptate possimus.</p>
+      
     </>
   );
 }
