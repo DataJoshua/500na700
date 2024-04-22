ba0ad32a3ee50f59a1435233773a7c1e3f42d58c
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-04-22 21:22:02 +0400

    init and normalize layout

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Hello There</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quaerat voluptate possimus.</p>
+    </>
+  );
+}
+
+export default App;
