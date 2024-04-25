ae37e46e6548783a67ad2ca14e79d0d7ab662ea5
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-04-26 01:00:10 +0400

    FAQ secction created

--- src/components/Faq/index.jsx
+++ src/components/Faq/index.jsx
@@ -1,8 +1,19 @@
+import "./Faq.scss";
+import FaqElement from "./components/FaqElement";
+
 function Faq() {
   return ( 
     <>
-      <div>
-        
+      <div className="faq__container">
+        <div className="faq__title-wrapper">
+          <h1 className="faq__title">FAQ</h1>
+        </div>
+
+        <div className="faq__list">
+          <FaqElement/>
+          <FaqElement/>
+          <FaqElement/>
+        </div>
       </div>
     </>
   )
